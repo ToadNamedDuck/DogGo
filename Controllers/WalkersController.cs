6e0a8b9903cc79271a3be93f3e5c0e3fd071d9b6
Jason Harris <lord.jtharris@gmail.com>
2023-06-19 11:28:03 -0500

    Init plus 2 repos and 2 models.

---
+++ Controllers/WalkersController.cs
@@ -0,0 +1,91 @@
+﻿using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogGo.Controllers
+{
+    public class WalkersController : Controller
+    {
+        private readonly IWalkerRepository _walkerRepo;
+
+        public WalkersController(IWalkerRepository walkerRepo)
+        {
+            _walkerRepo = walkerRepo;
+        }
+
+        // GET: WalkersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: WalkersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WalkersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WalkersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalkersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WalkersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalkersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WalkersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
