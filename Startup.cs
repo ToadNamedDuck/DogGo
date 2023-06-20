62c2c6e1a28f85670a14ec5e8211b4150db78d69
Jason Harris <lord.jtharris@gmail.com>
2023-06-20 10:23:06 -0500

    Added new nav links. Fixed owner details. Startup knows the OwnerRepository.

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace DogGo
         {
             services.AddControllersWithViews();
             services.AddTransient<IWalkerRepository, WalkerRepository>();
+            services.AddTransient<IOwnerRepository, OwnerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
